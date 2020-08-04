929041c0db7ee42bf3b22c6eadfb1fa218292630
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-04 01:05:02 -0300

    feat: create logic to transform a message in code

---
+++ src/entities/message/Message.js
@@ -0,0 +1,32 @@
+const keyboard = [
+  { value: '0', letters: [' '] },
+  { value: '2', letters: ['A', 'B', 'C'] },
+  { value: '3', letters: ['D', 'E', 'F'] },
+  { value: '4', letters: ['G', 'H', 'I'] },
+  { value: '5', letters: ['J', 'K', 'L'] },
+  { value: '6', letters: ['M', 'N', 'O'] },
+  { value: '7', letters: ['P', 'Q', 'R', 'S'] },
+  { value: '8', letters: ['T', 'U', 'V'] },
+  { value: '9', letters: ['W', 'X', 'Y', 'Z'] }
+]
+
+const separator = '_'
+
+const msg = 'TESTE DE MESA'
+
+const sms = msg
+  .toUpperCase()
+  .split('')
+  .map((character, index, elements) => {
+    const equivalentKey = keyboard.find((key) => key.letters.includes(character))
+    const position = equivalentKey.letters.findIndex((letter) => letter === character)
+
+    const repetitions = equivalentKey.value.repeat(position + 1)
+
+    const next = elements[index + 1]
+
+    return equivalentKey.letters.includes(next) ? repetitions + separator : repetitions
+  })
+  .join('')
+
+console.log(sms)
