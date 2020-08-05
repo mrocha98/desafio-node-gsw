4ca71d56b62281639ef470760af1499b8a4507b6
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-05 03:30:15 -0300

    refactor: import MongoDB connection in app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+try {
+  require('./database/connection')
+} catch {
+  process.exit(1)
+}
 const Koa = require('koa')
 const bodyParser = require('koa-body')
 const helmet = require('koa-helmet')
@@ -6,17 +11,16 @@ const logger = require('koa-logger')
 
 const router = require('./routes')
 
-const { NODE_ENV, SERVER_PORT } = process.env
+const { SERVER_PORT } = process.env
 
 const app = new Koa()
 
 app.use(bodyParser())
 app.use(helmet())
 app.use(cors())
-
-if (NODE_ENV === 'development') app.use(logger())
+app.use(logger())
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen(SERVER_PORT, () => console.log(`🚀 Server listening on ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`⚡ server listening on ${SERVER_PORT}`))
