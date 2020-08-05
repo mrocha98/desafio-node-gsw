8ca219e7096c6949746f694e5ccd896f59818c00
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-05 02:21:55 -0300

    refactor: organize app to use middlewares and routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const Koa = require('koa')
+const bodyParser = require('koa-body')
 const helmet = require('koa-helmet')
+const cors = require('@koa/cors')
+const logger = require('koa-logger')
+
+const router = require('./routes')
+
+const { NODE_ENV, SERVER_PORT } = process.env
+
 const app = new Koa()
 
+app.use(bodyParser())
 app.use(helmet())
+app.use(cors())
 
-app.use((ctx) => {
-  ctx.body = 'Hey Hey!'
-})
+if (NODE_ENV === 'development') app.use(logger())
 
-app.listen(process.env.SERVER_PORT)
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(SERVER_PORT, () => console.log(`🚀 Server listening on ${SERVER_PORT}`))
