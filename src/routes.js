167519cfa18a2b610d314643ee2dcfdd725dc195
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-06 02:45:48 -0300

    refactor: use validAuth middleware in all routes except login e user creation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const Router = require('koa-router')
 
+const validateAuth = require('./middlewares/validateAuth')
+
 const AuthController = require('./entities/auth/AuthController')
 const UserController = require('./entities/user/UserController')
 const MessageController = require('./entities/message/MessageController')
@@ -8,6 +10,8 @@ const router = new Router()
 
 router.post('/user', UserController.create)
 router.post('/login', AuthController.login)
+
+router.use(validateAuth)
 
 router.delete('/user/:id', UserController.destroy)
 
